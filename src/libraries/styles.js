a41cd57ba189cc9caaa4a84b99f9854cce1a1ef3
krystof1119 <krystof1119@gmail.com>
2018-11-20 19:56:00 +0100

    Create styles.js

---
+++ src/libraries/styles.js
@@ -0,0 +1,26 @@
+const styles = {
+  button: {
+    font: "1rem 'Fira Sans', sans-serif",
+    padding: '.5rem',
+    backgroundColor: 'royalblue',
+    border: 'none',
+    borderRadius: 4,
+    margin: 4,
+    color: 'white',
+  },
+  slider: {
+    width: '100%',
+    height: 15,
+    borderRadius: 5,
+    backgroundColor: '#d3d3d3',
+    outline: 'none',
+    opacity: 0.7,
+    transition: 'opacity .2s',
+  },
+  columns: {
+    2: {
+      width: '99%',
+    },
+  },
+};
+export default styles;
